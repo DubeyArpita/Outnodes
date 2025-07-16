b4061d16a4acdbc7a80ee0dd0379a1a74354f148
DubeyArpita <arpitadubey23216@gmail.com>
2025-07-16 23:27:35 +0530

    app changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react'
-import './App.css'
-import { Outlet } from 'react-router-dom'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import { useState, useEffect } from "react";
+import "./App.css";
+import { Outlet } from "react-router-dom";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import { useDispatch } from "react-redux";
+import { loginUser } from "./store/userSlice";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    const storedUser = localStorage.getItem("user");
+    const role = localStorage.getItem("role");
+
+    if (token && storedUser) {
+      try {
+        const parsedUser = JSON.parse(storedUser);
+        dispatch(
+          loginUser({
+            user: parsedUser,
+            role,
+            token,
+          })
+        );
+      } catch (err) {
+        console.error("Failed to parse stored user:", err);
+      }
+    }
+  }, [dispatch]);
 
   return (
     <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
@@ -20,4 +43,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
